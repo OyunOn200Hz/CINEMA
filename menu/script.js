d02f0fd4ca850271a462b0dd58ca317f5b3bbcb0
OyunOn200Hz <panfil.noa@gmail.com>
2025-01-15 00:18:13 +0100

    v4
    - refonte du js
    - affiche description du film sur le hover de l'affiche
    - commencement du load more films

--- menu/script.js
+++ menu/script.js
@@ -1,18 +1,33 @@
-function fetchAndDisplayMovie(movieTitle, posterElementId, titleElementId) {
-    fetch(`https://www.omdbapi.com/?apikey=586caa23&s=${encodeURIComponent(movieTitle)}`)
+function fetchAndDisplayMovie(idFilms, posterElementId, descriptionFilm, titleElementId) {
+    fetch(`https://www.omdbapi.com/?apikey=586caa23&i=${(idFilms)}`)
         .then(response => response.json())
         .then(data => {
-            if (data.Search && data.Search.length > 0) {
-                const moviePoster = data.Search[0].Poster;
-                document.getElementById(posterElementId).style.backgroundImage = `url(${moviePoster})`;
-                document.getElementById(titleElementId).innerHTML = data.Search[0].Title;
-            } else {
-                console.error('Pas de Résultats trouvé pour : ', movieTitle);
+            const moviePoster = data.Poster;
+            document.getElementById(posterElementId).style.backgroundImage = `url(${moviePoster})`;
+            document.getElementById(titleElementId).innerHTML = data.Title;
+            document.getElementById(posterElementId).setAttribute('film-description', data.Plot);
+            if (!data.Poster) {
+                console.error('Pas de Résultats trouvé pour : ', idFilms);
             }
         })
-        .catch(error => console.error('Error fetching data for', movieTitle, ':', error));
+        .catch(error => console.error('Error fetching data for', idFilms, ':', error));
 }
 
-fetchAndDisplayMovie('Interstellar', 'AfficheFilm1', 'NomFilm1');
-fetchAndDisplayMovie('Oppenheimer', 'AfficheFilm2', 'NomFilm2');
-fetchAndDisplayMovie('Fight Club', 'AfficheFilm3', 'NomFilm3');
+fetchAndDisplayMovie('tt0816692', 'AfficheFilm1', 'AfficheFilm1', 'NomFilm1'); //Interstellar
+fetchAndDisplayMovie('tt15398776', 'AfficheFilm2', 'AfficheFilm2', 'NomFilm2'); //Oppenheimer
+fetchAndDisplayMovie('tt0137523', 'AfficheFilm3', 'AfficheFilm3', 'NomFilm3'); //Fight Club
+
+
+const listeFilms2024 = ["tt26446278",//Le comte de monte cristo
+                        "tt15239678",//Dune 2
+                        "tt12037194", //Furiosa
+                        "tt13186482", //Mufasa : Le roi lion
+                        "tt4772188", //Flow
+                        "tt1684562", //The fall guy
+                        "tt17526714", //The substance
+                        "tt1262426", //Wicked
+                        "tt10462154", //La plus précieuse des marchandises
+                        "tt30795948", //Un petit truc en plus
+                        "tt27490099", //L'amour ouf
+                        "tt18259086" //Sonic 3                        
+];
--- menu/script2.js
+++ menu/script2.js
@@ -1,54 +1,27 @@
-function fetchAndDisplayMovie(movieTitle, posterElementId, titleElementId, voletElementId) {
-    fetch(`https://www.omdbapi.com/?apikey=586caa23&t=${encodeURIComponent(movieTitle)}`)
+function fetchAndDisplayMovie(idFilms, posterElementId, descriptionFilm, titleElementId) {
+    fetch(`https://www.omdbapi.com/?apikey=586caa23&i=${(idFilms)}`)
         .then(response => response.json())
         .then(data => {
-            if (data.Response === "True") {
-                const moviePoster = data.Poster;
-                const movieType = data.Genre;
-                const movieReleaseDate = data.Released;
-                document.getElementById(posterElementId).style.backgroundImage = `url(${moviePoster})`;
-                document.getElementById(titleElementId).innerHTML = data.Title;
-                document.getElementById(voletElementId).innerHTML = `Type: ${movieType}<br>Date de sortie: ${movieReleaseDate}`;
-            } else {
-                console.error('Pas de Résultats trouvé pour : ', movieTitle);
+            const moviePoster = data.Poster;
+            document.getElementById(posterElementId).style.backgroundImage = `url(${moviePoster})`;
+            document.getElementById(titleElementId).innerHTML = data.Title;
+            document.getElementById(posterElementId).setAttribute('film-description', data.Plot);
+            if (!data.Poster) {
+                console.error('Pas de Résultats trouvé pour : ', idFilms);
             }
         })
-        .catch(error => console.error('Error fetching data for', movieTitle, ':', error));
+        .catch(error => console.error('Error fetching data for', idFilms, ':', error));
 }
 
-fetchAndDisplayMovie('Interstellar', 'AfficheFilm1', 'NomFilm1', 'Volet1');
-fetchAndDisplayMovie('Oppenheimer', 'AfficheFilm2', 'NomFilm2', 'Volet2');
-fetchAndDisplayMovie('Fight Club', 'AfficheFilm3', 'NomFilm3', 'Volet3');
+fetchAndDisplayMovie('tt26446278', 'AfficheFilm1', 'AfficheFilm1', 'NomFilm1'); //Interstellar
+fetchAndDisplayMovie('tt15398776', 'AfficheFilm2', 'AfficheFilm2', 'NomFilm2'); //Oppenheimer
+fetchAndDisplayMovie('tt0137523', 'AfficheFilm3', 'AfficheFilm3', 'NomFilm3'); //Fight Club
 
-function fetchAndDisplayMoviesByYear(year, startIndex) {
-    fetch(`https://www.omdbapi.com/?apikey=586caa23&s=&y=${year}`)
-        .then(response => response.json())
-        .then(data => {
-            if (data.Response === "True" && data.Search) {
-                data.Search.slice(startIndex, startIndex + 9).forEach((movie, index) => {
-                    const filmId = `film${startIndex + index + 4}`;
-                    const afficheId = `AfficheFilm${startIndex + index + 4}`;
-                    const nomId = `NomFilm${startIndex + index + 4}`;
-                    const voletId = `Volet${startIndex + index + 4}`;
 
-                    const filmDiv = document.createElement('div');
-                    filmDiv.classList.add('film');
-                    filmDiv.innerHTML = `
-                        <div class="AfficheFilm" id="${afficheId}"></div>
-                        <div class="NomFilm" id="${nomId}"></div>
-                        <div class="Volet" id="${voletId}"></div>
-                    `;
-                    document.getElementById('EnAvant').appendChild(filmDiv);
-
-                    fetchAndDisplayMovie(movie.Title, afficheId, nomId, voletId);
-                });
-            } else {
-                console.error('Pas de Résultats trouvé pour l\'année : ', year);
-            }
-        })
-        .catch(error => console.error('Error fetching data for year', year, ':', error));
-}
-
-document.getElementById('loadMoreBtn').addEventListener('click', () => {
-    fetchAndDisplayMoviesByYear(2024, 0);
-});
+const listeFilms2024 = ["tt26446278",//Le comte de monte cristo
+                        "tt15239678",//Dune 2
+                        "tt12037194", //Furiosa
+                        "tt13186482", //Mufasa : Le roi lion
+                        "", 
+                        
+];
