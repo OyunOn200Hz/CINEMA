bf1c158c5cd05c85bdd989a1c801a20e039a6a61
OyunOn200Hz <panfil.noa@gmail.com>
2025-01-18 16:41:56 +0100

    v5
    ajout films 2024

--- menu/script2.js
+++ menu/script2.js
@@ -13,7 +13,7 @@ function fetchAndDisplayMovie(idFilms, posterElementId, descriptionFilm, titleEl
         .catch(error => console.error('Error fetching data for', idFilms, ':', error));
 }
 
-fetchAndDisplayMovie('tt26446278', 'AfficheFilm1', 'AfficheFilm1', 'NomFilm1'); //Interstellar
+fetchAndDisplayMovie('tt0816692', 'AfficheFilm1', 'AfficheFilm1', 'NomFilm1'); //Interstellar
 fetchAndDisplayMovie('tt15398776', 'AfficheFilm2', 'AfficheFilm2', 'NomFilm2'); //Oppenheimer
 fetchAndDisplayMovie('tt0137523', 'AfficheFilm3', 'AfficheFilm3', 'NomFilm3'); //Fight Club
 
@@ -22,6 +22,12 @@ const listeFilms2024 = ["tt26446278",//Le comte de monte cristo
                         "tt15239678",//Dune 2
                         "tt12037194", //Furiosa
                         "tt13186482", //Mufasa : Le roi lion
-                        "", 
-                        
+                        "tt4772188", //Flow
+                        "tt1684562", //The fall guy
+                        "tt17526714", //The substance
+                        "tt1262426", //Wicked
+                        "tt10462154", //La plus précieuse des marchandises
+                        "tt30795948", //Un petit truc en plus
+                        "tt27490099", //L'amour ouf
+                        "tt18259086" //Sonic 3                        
 ];
